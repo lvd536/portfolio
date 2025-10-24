e00979a71e6b56597e344d5d800c94a95386f310
lvd. <nbikiti@gmail.com>
2025-10-24 17:01:01 +0400

    - Implemented all anims

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -1,9 +1,9 @@
 export default function Title() {
     return (
-        <div className="text-4xl">
-            <span className="text-indigo-500">Hello!</span>
-            <span className="text-black dark:text-white"> My name is lvd.</span>
-            <span className='block text-black dark:text-white'> I can do some great things for you.</span>
+        <div className="text-4xl py-2 animate-custom-typing overflow-hidden self-center">
+            <span className="text-indigo-500 whitespace-nowrap">Hello!</span>
+            <span className="text-black dark:text-white whitespace-nowrap"> My name is lvd.</span>
+            <span className='block text-black dark:text-white whitespace-nowrap'> I can do some great things for you.</span>
         </div>
     )
 }
--- src/components/Skills/SkillCard.tsx
+++ src/components/Skills/SkillCard.tsx
@@ -4,7 +4,7 @@ export default function SkillCard({header, children} : {header: string, children
     return (
         <div>
             <Header word1={header} word2={'skills'}/>
-            <ul className="flex flex-col gap-6">
+            <ul className="flex flex-col gap-6 animate-custom-bounceX-alternative">
                 {children}
             </ul>
         </div>
