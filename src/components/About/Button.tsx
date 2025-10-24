aabfec074e6a07cbf3068489683afcc4e5ae5052
lvd. <nbikiti@gmail.com>
2025-10-24 14:53:01 +0400

    - Add hover about button effect

--- src/components/About/Button.tsx
+++ src/components/About/Button.tsx
@@ -1,5 +1,5 @@
 export default function Button({children}: {children: React.ReactNode}) {
     return (
-        <a className="flex w-35 h-10 shadow-(--box-shadow-custom-2) rounded-xl mt-10 p-2 items-center justify-center text-black dark:text-white" href='#contacts'>{children}</a>
+        <a className="flex w-35 h-10 shadow-(--box-shadow-custom-2) rounded-xl mt-10 p-2 items-center justify-center text-black dark:text-white hover:ring-1 ring-indigo-500" href='#contacts'>{children}</a>
     )
 }
