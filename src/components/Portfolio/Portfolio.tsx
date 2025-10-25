9a6def832af2b2cb64f0e417dc306de38677ae70
lvd. <nbikiti@gmail.com>
2025-10-25 19:09:48 +0400

    - Added real projects into portfolio

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -8,7 +8,9 @@ import zustand from '../../assets/langs/zustand.svg'
 import telegram from '../../assets/langs/telegram.svg'
 import react from '../../assets/langs/react.svg'
 import database from '../../assets/langs/database.svg'
-import testCase from '../../assets/portfolio/testCase.jpg'
+import tailwind from '../../assets/langs/tailwind.svg'
+import shovel from '../../assets/portfolio/shovel.jpg'
+import portfolio from '../../assets/portfolio/portfolio.jpg'
 import PortfolioCard from "./PortfolioCard.tsx";
 import Header from "../Header.tsx";
 import Modal from './Modal/Modal.tsx'
@@ -20,20 +22,20 @@ export default function Portfolio() {
         <section id="portfolio" className="mt-25">
             <Header word1={'My'} word2={'portfolio'}/>
             <PortfolioCard
-                skills={[html, css, scss, typescript, javascript, react, csharp]}
-                header='Test Header'
-                dateTime='2025-08'
-                dateString='August 2025'
-                description='Minimalistic form of authorization based on SPA with integration of several technologies: Vite, TypeScript, React, React Router, Redux, Redux Toolkit, Redux Persist, Formik, Yup, classNames, PostCSS, Vite SVG icon, Vitest etc.'
-                projectImage={testCase}
+                skills={[html, css, tailwind, typescript, javascript, react, database, zustand]}
+                header='Portfolio'
+                dateTime='2025-10'
+                dateString='October 2025'
+                description='Minimalistic portfolio based on SPA with integration of several technologies: Vite, TypeScript, React, Html. CSS, Tailwind 4, Zustand etc.'
+                projectImage={portfolio}
             />
             <PortfolioCard
                 skills={[html, css, scss, typescript, javascript, react, database, zustand, telegram]}
-                header='Test Header 2'
+                header='Hamster Combat Clone'
                 dateTime='2025-09'
                 dateString='September 2025'
-                description='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic'
-                projectImage={testCase}
+                description='Hamster Combat V1 Telegram WebApp clone with integration of several technologies: Vite, TypeScript, React, Html. CSS, SCSS, Zustand, Supabase etc.'
+                projectImage={shovel}
             />
             {isModal && <Modal/>}
         </section>
