087fc6803fa953c17e86bfab468e21b9f40ec19e
lvd. <nbikiti@gmail.com>
2025-10-25 19:01:05 +0400

    - New Animations and simple fixes - Add open animation on image click - Add hover effect for Portfolio Image - Add object fit on Portfolio image

--- src/components/Portfolio/Modal/Modal.tsx
+++ src/components/Portfolio/Modal/Modal.tsx
@@ -7,7 +7,7 @@ export default function Modal() {
     })
     return (
     <div className="flex items-center justify-center fixed top-0 left-0 w-full h-full bg-black/50 z-40" onClick={handleClick}>
-        <img src={image} alt="123"  className="w-1/2 h-auto z-30"/>
+        <img src={image} alt="123"  className="w-1/2 h-auto z-30 animate-custom-modal"/>
     </div>
   )
 }
--- src/components/Portfolio/PortfolioCard.tsx
+++ src/components/Portfolio/PortfolioCard.tsx
@@ -15,7 +15,7 @@ export default function PortfolioCard({header, dateTime, dateString, description
     }
     return (
         <div className="flex flex-col lg:flex-row bg-custom-light dark:bg-bg-dark gap-[10vw] mt-10 transition-shadow duration-300 shadow-(--box-shadow-custom-2) hover:shadow-(--box-shadow-custom-hover) rounded-xl p-10 animate-custom-bounceY" onClick={handleClick}>
-            <img src={projectImage} alt="" className="w-full lg:max-w-100 max-h-100 rounded-lg"/>
+            <img src={projectImage} alt="" className="w-full lg:max-w-100 max-h-100 rounded-lg duration-300 hover:ring-1 ring-indigo-500/60 object-cover"/>
             <div className="flex flex-col w-full gap-8">
                 <time className="w-fit text-indigo-500 font-medium shadow-(--box-shadow-inset) rounded-2xl p-2 px-5" dateTime={dateTime}>{dateString}</time>
                 <span className="text-4xl text-black dark:text-white">{header}</span>
