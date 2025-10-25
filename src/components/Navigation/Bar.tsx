67bb31468102b68019bfd95bc291ad8a53c7a42a
lvd. <nbikiti@gmail.com>
2025-10-25 22:33:45 +0400

    - Add burger menu

---
+++ src/components/Navigation/Bar.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import NavButton from "./NavButton";
+
+export default function Bar() {
+  const [isBurgerOpen, setIsBurgerOpen] = useState<boolean>(false);
+  const handleClick = () => {
+    setIsBurgerOpen((prev) => {
+      return !prev;
+    });
+  };
+  return (
+    <div>
+      {!isBurgerOpen && <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+        onClick={handleClick}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>}
+      {isBurgerOpen && 
+      <div 
+      className="fixed top-0 left-0 flex flex-col items-center justify-center text-3xl gap-10 w-full h-full bg-bg dark:bg-bg-dark z-2"
+      onClick={handleClick}
+      >
+            <NavButton section={'about'}>About</NavButton>
+            <NavButton section={'skills'}>Skills</NavButton>
+            <NavButton section={'portfolio'}>Portfolio</NavButton>
+            <NavButton section={'contacts'}>Contacts</NavButton>
+        </div>
+      }
+    </div>
+  );
+}
--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -2,6 +2,7 @@ import NavButton from "./NavButton.tsx";
 import Logo from "./Logo.tsx";
 import ThemeToggle from "./ThemeToggle.tsx";
 import {useEffect, useState} from "react";
+import Bar from "./Bar.tsx";
 
 export default function NavBar() {
     const [isScrolled, setIsScrolled] = useState<boolean>(false)
@@ -17,13 +18,14 @@ export default function NavBar() {
         <div className={`fixed z-2  w-full left-0 ${isScrolled ? 'pt-0' : 'pt-5'} ${isScrolled && 'bg-bg dark:bg-bg-dark shadow-lg shadow-black/50 transition-shadow transition-padding duration-600'}`}>
             <div className={`flex items-center justify-between container mx-auto`}>
                 <Logo/>
-                <div className="flex gap-10">
+                <div className="hidden sm:flex gap-10">
                     <NavButton section={'about'}>About</NavButton>
                     <NavButton section={'skills'}>Skills</NavButton>
                     <NavButton section={'portfolio'}>Portfolio</NavButton>
                     <NavButton section={'contacts'}>Contacts</NavButton>
                 </div>
                 <ThemeToggle/>
+                <Bar/>
             </div>
         </div>
     )
