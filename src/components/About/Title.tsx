fee76aa1c9ade09da8128de1af7b8df61989403c
lvd. <nbikiti@gmail.com>
2025-10-25 22:41:05 +0400

    - Styles changes - root: overflow: hodden - Title.tsx: change text sizing

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
     return (
-        <div className="text-4xl py-2 animate-custom-typing overflow-hidden self-center">
+        <div className="text-2xl sm:text-4xl py-2 animate-custom-typing overflow-hidden self-center">
             <span className="text-indigo-500 whitespace-nowrap">Hello!</span>
             <span className="text-black dark:text-white whitespace-nowrap"> My name is lvd.</span>
             <span className='block text-black dark:text-white whitespace-nowrap'> I can do some great things for you.</span>
