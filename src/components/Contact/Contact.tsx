3ed4c861521e30bba3254e81cda3de82ff2d86fe
lvd. <nbikiti@gmail.com>
2025-10-25 22:49:11 +0400

    - Adaptive for contracts page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -7,10 +7,10 @@ export default function Contact() {
     return (
         <section id="contacts">
             <div className="bg-custom-light dark:bg-bg-dark shadow-(--box-shadow-custom-1) w-full flex flex-col gap-8 relative rounded-4xl px-8 py-12 my-15" id="contact">
-                <div className="flex items-center justify-between">
+                <div className="flex flex-col sm:flex-row sm:items-center justify-between">
                     <div className="flex flex-col">
-                        <span className="text-4xl text-black dark:text-white">Do you want to ask</span>
-                        <span className="text-indigo-500 text-4xl">something interesting?</span>
+                        <span className="text-3xl sm:text-4xl text-black dark:text-white">Do you want to ask</span>
+                        <span className="text-indigo-500 text-3xl sm:text-4xl">something interesting?</span>
                     </div>
                     <ul className="flex gap-5">
                         <a href="https://github.com/lvd536">
@@ -21,7 +21,7 @@ export default function Contact() {
                         </a>
                     </ul>
                 </div>
-                <div className="flex items-center justify-between">
+                <div className="flex flex-col sm:flex-row sm:items-center justify-between">
                     <span className="text-s text-black dark:text-white">Contact me. I am online mon-fri from 8 am to 8 pm (gmt).</span>
                     <span className="text-s text-black dark:text-white">© lvd. 2025</span>
                 </div>
