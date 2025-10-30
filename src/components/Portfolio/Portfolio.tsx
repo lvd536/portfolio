0d456cdeaa3b559ac3d7c163c313ac1c7b2914c2
lvd. <nbikiti@gmail.com>
2025-10-30 14:57:01 +0400

    - Rework & refactor portfolio - Add portfolio card interface - Add array of card in Portfolio.tsx - Add dynamic card node loading - Other simple changes

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -4,37 +4,45 @@ import * as portfolioImages from '../../assets/portfolio/index.ts'
 import PortfolioCard from "./PortfolioCard.tsx";
 import Header from "../Header.tsx";
 import Modal from './Modal/Modal.tsx'
+import type { IPortfolioCard } from './PortfolioCard.tsx';
 import { useIsModal } from '../../store/useModalStore.tsx'
 
 export default function Portfolio() {
     const isModal = useIsModal()
-    return (
+    const portfolioProjects: IPortfolioCard[] = [
+        {
+            skills: [langs.html, langs.css, langs.tailwind, langs.typescript, langs.javascript, langs.react, langs.database, langs.zustand],
+            header: 'Portfolio',
+            dateTime: '2025-10',
+            dateString: 'October 2025',
+            description: 'Minimalistic portfolio based on SPA with integration of several technologies: Vite, TypeScript, React, Html. CSS, Tailwind 4, Zustand etc.',
+            projectImage: portfolioImages.portfolio,
+        },
+        {
+            skills: [langs.html, langs.css, langs.tailwind, langs.typescript, langs.javascript, langs.react, langs.database, langs.zustand],
+            header: 'Hamster Combat Clone',
+            dateTime: '2025-09',
+            dateString: 'September 2025',
+            description: 'Hamster Combat V1 Telegram WebApp clone with integration of several technologies: Vite, TypeScript, React, Html. CSS, SCSS, Zustand, Supabase etc.',
+            projectImage: portfolioImages.shovel,
+        },
+        {
+            skills: [langs.html, langs.css, langs.tailwind, langs.typescript, langs.javascript, langs.react, langs.database, langs.zustand],
+            header: 'To-Do List',
+            dateTime: '2025-10',
+            dateString: 'October 2025',
+            description: 'Minimalistic todo list app based on SPA with integration of several technologies: Vite, TypeScript, React, Html. CSS, Tailwind 4, Zustand etc.',
+            projectImage: portfolioImages.todoList,
+        }
+    ]
+   return (
         <section id="portfolio" className="mt-25">
             <Header word1={'My'} word2={'portfolio'}/>
-            <PortfolioCard
-                skills={[langs.html, langs.css, langs.tailwind, langs.typescript, langs.javascript, langs.react, langs.database, langs.zustand]}
-                header='Portfolio'
-                dateTime='2025-10'
-                dateString='October 2025'
-                description='Minimalistic portfolio based on SPA with integration of several technologies: Vite, TypeScript, React, Html. CSS, Tailwind 4, Zustand etc.'
-                projectImage={portfolioImages.portfolio}
-            />
-            <PortfolioCard
-                skills={[langs.html, langs.css, langs.scss, langs.typescript, langs.javascript, langs.react, langs.database, langs.zustand, langs.telegram]}
-                header='Hamster Combat Clone'
-                dateTime='2025-09'
-                dateString='September 2025'
-                description='Hamster Combat V1 Telegram WebApp clone with integration of several technologies: Vite, TypeScript, React, Html. CSS, SCSS, Zustand, Supabase etc.'
-                projectImage={portfolioImages.shovel}
-            />
-            <PortfolioCard
-                skills={[langs.html, langs.css, langs.typescript, langs.javascript, langs.react, langs.zustand]}
-                header='To-Do List'
-                dateTime='2025-10'
-                dateString='October 2025'
-                description='Minimalistic todo list app based on SPA with integration of several technologies: Vite, TypeScript, React, Html. CSS, Tailwind 4, Zustand etc.'
-                projectImage={portfolioImages.todoList}
-            />
+            {portfolioProjects.map((portfolio, index) => {
+                return (
+                    <PortfolioCard portfolioDetails={portfolio} key={index}/>
+                )
+            })}
             {isModal && <Modal/>}
         </section>
     )
--- src/components/Portfolio/PortfolioCard.tsx
+++ src/components/Portfolio/PortfolioCard.tsx
@@ -1,27 +1,29 @@
 import { setImage, setIsModal } from "../../store/useModalStore.tsx";
 import Skill from "../Skill.tsx";
 
-export default function PortfolioCard({header, dateTime, dateString, description, skills, projectImage}: {
+export interface IPortfolioCard {
     header: string,
     dateTime: string,
     dateString: string,
     description: string,
     skills: string[],
     projectImage: string
-}) {
+}
+
+export default function PortfolioCard({portfolioDetails}: {portfolioDetails: IPortfolioCard}) {
     const handleClick = () => {
-        setImage(projectImage)
+        setImage(portfolioDetails.projectImage)
         setIsModal(true)
     }
     return (
         <div className="flex flex-col lg:flex-row bg-custom-light dark:bg-bg-dark gap-[10vw] mt-10 transition-shadow duration-300 shadow-(--box-shadow-custom-2) hover:shadow-(--box-shadow-custom-hover) rounded-xl p-10 animate-custom-bounceY" onClick={handleClick}>
-            <img src={projectImage} alt="" className="w-full lg:max-w-100 max-h-100 rounded-lg duration-300 hover:ring-1 ring-indigo-500/60 object-cover"/>
+            <img src={portfolioDetails.projectImage} alt="" className="w-full lg:max-w-100 max-h-100 rounded-lg duration-300 hover:ring-1 ring-indigo-500/60 object-cover"/>
             <div className="flex flex-col w-full gap-8">
-                <time className="w-fit text-indigo-500 font-medium shadow-(--box-shadow-inset) rounded-2xl p-2 px-5" dateTime={dateTime}>{dateString}</time>
-                <span className="text-4xl text-black dark:text-white">{header}</span>
-                <span className="text-black dark:text-white">{description}</span>
+                <time className="w-fit text-indigo-500 font-medium shadow-(--box-shadow-inset) rounded-2xl p-2 px-5" dateTime={portfolioDetails.dateTime}>{portfolioDetails.dateString}</time>
+                <span className="text-4xl text-black dark:text-white">{portfolioDetails.header}</span>
+                <span className="text-black dark:text-white">{portfolioDetails.description}</span>
                 <ul className="flex flex-wrap gap-4">
-                    {skills.map((skill, index) => ( 
+                    {portfolioDetails.skills.map((skill, index) => ( 
                         <Skill key={index} image={skill}/>
                     ))}
                 </ul>
